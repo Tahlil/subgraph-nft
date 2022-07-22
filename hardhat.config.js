6b103943bc14f08fd509c3852da00386639bd16b
tahlil <bsse0803@iit.du.ac.bd>
2022-07-22 21:49:19 +0600

    Update hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,11 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+// Load the Hardhat plugins
+require('@nomiclabs/hardhat-waffle');
+require('hardhat-abi-exporter');
+
 module.exports = {
-  solidity: "0.8.9",
+  solidity: '0.8.9',
+  abiExporter: {
+    path: './abi/',
+    clear: true,
+  },
 };
